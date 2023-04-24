444f92f2b6a97ee0507ddc53b4405774c343b3c9
AshleyVazCo <Ashley.vazquezcolon@gmail.com>
2023-04-24 15:14:33 -0400

    new commit
    new commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # NotesApp
  Porfolio assignment for DIG4639C - Mobile Development
+
+Option 2: New Project, 3 components
+Create ONE (1) new React Native project using React Native Elements and React Navigation (using Expo). It should include at least 3 screens and three new components that you author (not built-in components).
+
+Remember, a screen is a set of components visible on the screen. A screen is not the same set of components with different values with a different file name.
+
+Possibilities include the following, with approval required for other options:
+
+Create an alternative design for a fully-functioning exercise app with one exercise per screen with a central screen from which a user can navigate.
+This app must be substantially different than the one produced previously but may have similar features.
+New components could include the workout plan previously designed (e.g. specific exercises in a sequence),  
+Create a new study app or trivia app with new question types (other than multiple choice).
+Each question type would be its own screen, with the score tallied at the end.
+Examples for new question types would include text input (for math problems, for instance), drop down, matching, survey (e.g. a Likert scale for collecting opinions Links to an external site.). 
+Each question type should be reflected in the data structure and have a way to evaluate whether it is correct or completed (survey)
+Create a partial implementation of an e-commerce app, which might include a form for collecting info on selling an item (e.g. Amazon marketplace, or eBay) and mock up of sales listings. Think simple.
---
+++ notesApp/App.js
@@ -0,0 +1,362 @@
+import * as React from 'react';
+import {useRef} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { Pressable, ScrollView, StyleSheet, Text, Animated, PanResponder, View } from 'react-native';
+import { SpeedDial } from '@rneui/base';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { SpeedDialAction } from '@rneui/base/dist/SpeedDial/SpeedDial.Action';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Folder' component={FolderScreen} />
+        <Stack.Screen name='Notes' component={NotesScreen} />
+        <Stack.Screen name='Market' component={MarketScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({navigation}) {
+  const [open, setOpen] = React.useState(false);
+
+  return(
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+      <Text style={styles.homeText}>Welcome,</Text> 
+      <Text style={styles.homeText2}>Ashley</Text>
+
+      
+      <Pressable
+      onPress={() => navigation.navigate('Folder')}
+      style={styles.pressable}
+      ><Text style={styles.pressableText}>Folder</Text>
+      </Pressable>
+
+
+      <Pressable
+      onPress={() => navigation.navigate('Notes')}
+      style={styles.pressable}
+      ><Text style={styles.pressableText}>Notes</Text>
+      </Pressable>
+     
+
+      <Pressable
+      onPress={() => navigation.navigate('Market')}
+      style={styles.pressable}
+      ><Text style={styles.pressableText}>Market</Text>
+      </Pressable>
+
+      </ScrollView>
+
+
+      <SpeedDial
+      isOpen={open}
+      icon={{name:'add', color: 'black', overlayColor: '#dda0dd', size: 30}}
+      buttonStyle={{
+        backgroundColor: '#dda0dd',
+        borderRadius: 100,
+        width: 90,
+        height: 90
+      }}
+      openIcon={{name: 'close', color: 'black'}}
+      onOpen={() => setOpen(!open)}
+      onClose={() => setOpen(!open)}
+      >
+        <SpeedDialAction 
+        icon={{name: 'folder'}}
+        buttonStyle={{
+          backgroundColor: '#dda0dd',
+          borderRadius: 90,
+          width: 80,
+          height: 80,
+        }}
+        title="Create Folder"
+        onPress={() => navigation.navigate('Folder')}
+        />
+
+        <SpeedDialAction
+        icon={{name: 'note'}}
+        buttonStyle={{
+          backgroundColor: '#dda0dd',
+          borderRadius: 90,
+          width: 80,
+          height: 80,
+        }}
+        title="Add Note"
+        onPress={() => navigation.navigate('Notes')}
+        />
+
+      </SpeedDial>
+
+
+    <StatusBar style="auto" />
+    </SafeAreaView>
+  )
+}
+
+function FolderScreen({navigation}) {
+
+  //SpeedDial
+  const [open, setOpen] = React.useState(false);
+
+  //ScrollView
+  const [isScrolling, setScrolling] = React.useState(false);
+
+  return(
+    <SafeAreaView style={styles.container}>
+    <ScrollView
+    onScroll={(event) => {
+      setScrolling(event.nativeEvent.contentOffset.y > 0)
+    }}
+    >
+  
+    <Text style={styles.folderText}>My Folders</Text>
+
+      <Pressable
+      onPress={() => navigation.navigate('Notes')}
+      style={styles.folderPressable1}
+      ><Text style={styles.pressableText}>Mobile Dev</Text>
+      </Pressable>
+
+      <Pressable
+      onPress={() => navigation.navigate('Notes')}
+      style={styles.folderPressable2}
+      ><Text style={styles.pressableText}>Server Side</Text>
+      </Pressable>
+
+      <Pressable
+      onPress={() => navigation.navigate('Notes')}
+      style={styles.folderPressable3}
+      ><Text style={styles.pressableText}>Journal</Text>
+      </Pressable>
+
+      <Pressable
+      onPress={() => navigation.navigate('Notes')}
+      style={styles.folderPressable4}
+      ><Text style={styles.pressableText}>Art</Text>
+      </Pressable>
+
+      <Pressable
+      onPress={() => navigation.navigate('Notes')}
+      style={styles.folderPressable5}
+      ><Text style={styles.pressableText}>Social Media</Text>
+      </Pressable>
+
+      <Pressable
+      onPress={() => navigation.navigate('Market')}
+      style={styles.pressable}
+      ><Text style={styles.pressableText}>Market</Text>
+      </Pressable>
+      </ScrollView>
+
+      {!isScrolling && (
+
+      <SpeedDial
+      isOpen={open}
+      icon={{name:'add', color: 'black', overlayColor: '#dda0dd', size: 30}}
+      buttonStyle={{
+        backgroundColor: '#dda0dd',
+        borderRadius: 100,
+        width: 90,
+        height: 90
+      }}
+      openIcon={{name: 'close', color: 'black'}}
+      onOpen={() => setOpen(!open)}
+      onClose={() => setOpen(!open)}
+      >
+        <SpeedDialAction 
+        icon={{name: 'folder'}}
+        buttonStyle={{
+          backgroundColor: '#dda0dd',
+          borderRadius: 90,
+          width: 80,
+          height: 80,
+        }}
+        title="Create Folder"
+        onPress={() => navigation.navigate('Folder')}
+        />
+
+        <SpeedDialAction
+        icon={{name: 'edit'}}
+        buttonStyle={{
+          backgroundColor: '#dda0dd',
+          borderRadius: 90,
+          width: 80,
+          height: 80,
+        }}
+        title="Edit Folder"
+        onPress={() => navigation.navigate('Notes')}
+        />
+
+      </SpeedDial>
+       )}
+     
+
+    <StatusBar style="auto" />
+    </SafeAreaView>
+  )
+}
+
+function NotesScreen({navigation}) {
+
+  //SpeedDial
+  const [open, setOpen] = React.useState(false);
+
+  //ScrollView
+  const [isScrolling, setScrolling] = React.useState(false);
+
+
+  return(
+    <SafeAreaView style={styles.container}>
+
+    <ScrollView
+    onScroll={(event) => {
+      setScrolling(event.nativeEvent.contentOffset.y > 0)
+    }}
+    >
+    <Text>My Notes</Text>
+
+    </ScrollView>
+
+    {!isScrolling && (
+
+    <SpeedDial
+      isOpen={open}
+      icon={{name:'add', color: 'black', overlayColor: '#dda0dd', size: 30}}
+      buttonStyle={{
+        backgroundColor: '#dda0dd',
+        borderRadius: 100,
+        width: 90,
+        height: 90
+      }}
+      openIcon={{name: 'close', color: 'black'}}
+      onOpen={() => setOpen(!open)}
+      onClose={() => setOpen(!open)}
+      >
+        <SpeedDialAction 
+        icon={{name: 'edit'}}
+        buttonStyle={{
+          backgroundColor: '#dda0dd',
+          borderRadius: 90,
+          width: 80,
+          height: 80,
+        }}
+        title="Edit Note"
+        onPress={() => navigation.navigate('Folder')}
+        />
+
+        <SpeedDialAction
+        icon={{name: 'note'}}
+        buttonStyle={{
+          backgroundColor: '#dda0dd',
+          borderRadius: 90,
+          width: 80,
+          height: 80,
+        }}
+        title="Add Note"
+        onPress={() => navigation.navigate('Notes')}
+        />
+      </SpeedDial>
+    )}
+
+    <StatusBar style="auto" />
+    </SafeAreaView>
+  )
+}
+
+function MarketScreen({navigation}) {
+  return(
+    <SafeAreaView style={styles.container}>
+
+    <StatusBar style="auto" />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  homeText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  homeText2: {
+    fontSize: 40,
+    marginBottom: 20,
+  },
+  pressable: {
+    borderRadius: 8,
+    borderWidth: 1,
+    padding: 30,
+    marginTop: 10,
+    marginBottom: 20,
+    backgroundColor: '#2b2a28',
+  },
+  pressableText: {
+    textAlign: 'right',
+    fontSize: 35,
+    fontWeight: 'bold',
+    height: 40,
+    width: 250,
+    marginTop: 10,
+    color: 'white',
+  },
+  folderText: {
+    fontSize: 35,
+    marginRight: 150,
+    marginBottom: 20,
+  },
+  folderPressable1: {
+    borderRadius: 8,
+    borderWidth: 1,
+    padding: 30,
+    marginTop: 10,
+    marginBottom: 20,
+    backgroundColor: '#cb6efa',
+  },
+  folderPressable2: {
+    borderRadius: 8,
+    borderWidth: 1,
+    padding: 30,
+    marginTop: 10,
+    marginBottom: 20,
+    backgroundColor: '#6383ff',
+  },
+  folderPressable3: {
+    borderRadius: 8,
+    borderWidth: 1,
+    padding: 30,
+    marginTop: 10,
+    marginBottom: 20,
+    backgroundColor: '#5adba5',
+  },
+  folderPressable4: {
+    borderRadius: 8,
+    borderWidth: 1,
+    padding: 30,
+    marginTop: 10,
+    marginBottom: 20,
+    backgroundColor: '#e4aae6',
+  },
+  folderPressable5: {
+    borderRadius: 8,
+    borderWidth: 1,
+    padding: 30,
+    marginTop: 10,
+    marginBottom: 20,
+    backgroundColor: '#676391',
+  },
+});
